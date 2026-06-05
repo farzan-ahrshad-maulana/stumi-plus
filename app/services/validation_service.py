bee2d02559a44b49c3a77868f48c91592d9c6b17
farzan-ahrshad-maulana <fzan.ahrshad@gmail.com>
2026-06-05 20:39:48 +0700

    Update validation service, add validate_metadata function.

--- app/services/validation_service.py
+++ app/services/validation_service.py
@@ -1,6 +1,9 @@
+from app.schemas.metadata import LLMMetadata
+
+
 def basic_validation(
     text: str,
-):
+) -> tuple[bool, str]:
 
     if not text.strip():
         return False, "PDF contains no extractable text"
@@ -11,4 +14,20 @@ def basic_validation(
     if len(text.split()) < 1000:
         return False, "Not enough words"
 
-    return True, None
+    return True, ""
+
+
+def validate_metadata(
+    metadata: LLMMetadata,
+) -> tuple[bool, str]:
+
+    if len(metadata.title.strip()) < 10:
+        return False, "Invalid title"
+
+    if len(metadata.authors.strip()) < 5:
+        return False, "Invalid authors"
+
+    if len(metadata.abstract.strip()) < 300:
+        return False, "Abstract too short"
+
+    return True, ""
