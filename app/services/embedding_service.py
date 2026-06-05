004f2ab806152079885ed83c6d92cb7a5835e3b1
farzan-ahrshad-maulana <fzan.ahrshad@gmail.com>
2026-06-05 09:46:11 +0700

    Add chunk_text function.

--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -1,2 +1,36 @@
+from dataclasses import dataclass
+
 CHUNK_SIZE = 1000
 CHUNK_OVERLAP = 200
+
+
+@dataclass
+class TextChunk:
+    text: str
+    index: int
+
+
+def chunk_text(text: str) -> list[TextChunk]:
+
+    chunks: list[TextChunk] = []
+
+    start = 0
+    index = 0
+
+    while start < len(text):
+        end = start + CHUNK_SIZE
+
+        chunk = text[start:end]
+
+        chunks.append(
+            TextChunk(
+                text=chunk,
+                index=index,
+            )
+        )
+
+        start += CHUNK_SIZE - CHUNK_OVERLAP
+
+        index += 1
+
+    return chunks
