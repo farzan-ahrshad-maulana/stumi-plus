0e9706e5bb2f1afad379e68ee91fb1420b231066
Farzan Ahrshad Maulana <fzan.ahrshad@gmail.com>
2026-06-09 19:14:02 +0700

    Add CORS allow from vercel.

--- app/main.py
+++ app/main.py
@@ -30,6 +30,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://stumi-frontend.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
